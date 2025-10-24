167d1f09fd42e5772d780f935522116fb1799fc2
LuisDalcico <luisdalcico@outlook.com>
2025-10-24 10:07:23 -0300

    importando a outra funcao

--- funcoes.py
+++ funcoes.py
@@ -1,1 +1,11 @@
-print('hello')
+def define_posicoes(linha, coluna, orientacao, tamanho):
+    posicoes = []
+    
+    if orientacao == "vertical":
+        for i in range(tamanho):
+            posicoes.append([linha + i, coluna])
+    elif orientacao == "horizontal":
+        for i in range(tamanho):
+            posicoes.append([linha, coluna + i])
+    
+    return posicoes
