d098e74529310f650f3dfa14e96da569c8b422d1
LuisDalcico <luisdalcico@outlook.com>
2025-10-24 10:41:13 -0300

    DEFjogpart2

--- funcoes.py
+++ funcoes.py
@@ -23,3 +23,6 @@ def preenche_frota(frota, nome_navio, linha, coluna, orientacao, tamanho):
 def faz_jogada(tabuleiro, linha, coluna):
     if tabuleiro[linha][coluna] == 1:
         tabuleiro[linha][coluna] = 'X'
+    else:
+        tabuleiro[linha][coluna] = '-'
+    return tabuleiro
