c6a4a01b589f9f2d835f15a973218780c89b30b5
LuisDalcico <luisdalcico@outlook.com>
2025-10-24 10:17:41 -0300

    corpo funcFrota

--- preenchefrota.py
+++ preenchefrota.py
@@ -1,4 +1,11 @@
 import defineposicoes
 
 def preenche_frota(frota, nome_navio, linha, coluna, orientacao, tamanho):
+    posicoes = define_posicoes(linha, coluna, orientacao, tamanho)
     
+    if nome_navio not in frota:
+        frota[nome_navio] = [posicoes]
+    else:
+        frota[nome_navio].append(posicoes)
+    
+    return frota
