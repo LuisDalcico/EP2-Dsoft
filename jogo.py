6b9f9cccc61073a74b120efd76188bc08efc8b14
LuisDalcico <luisdalcico@outlook.com>
2025-10-24 16:33:55 -0300

    imports

--- jogo.py
+++ jogo.py
@@ -1,4 +1,9 @@
 from funcoes import define_posicoes
+from funcoes import preenche_frota
+from funcoes import faz_jogada
+from funcoes import posiciona_frota
+from funcoes import afundados
+
 
 
 linha = 2
@@ -7,4 +12,19 @@ orientacao = "vertical"
 tamanho = 3
 
 print(define_posicoes(linha, coluna, orientacao, tamanho))
+
+frota = {}
+nome_navio = 'navio-tanque'
+linha = 6
+coluna = 1
+orientacao = 'horizontal'
+tamanho = 3
+
+resultado = preenche_frota(frota, nome_navio, linha, coluna, orientacao, tamanho)
+print(resultado)
+{
+  "navio-tanque":[
+    [[6,1],[6,2],[6,3]]
+  ]
+}
 
